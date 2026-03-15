8e27b5bd1b1559a65df5651b5d7caccaf32cdc3c
BORGES <jessborgesb@gmail.com>
2026-03-15 15:04:34 +0100

    feat: add PiValueInterface and ReverseInterface functional interfaces for N1

---
+++ src/main/java/cat/itacademy/s1_08/n1/PiValueInterface.java
@@ -0,0 +1,6 @@
+package cat.itacademy.s1_08.n1;
+
+@FunctionalInterface
+public interface PiValueInterface {
+    double getPiValue();
+}
---
+++ src/main/java/cat/itacademy/s1_08/n1/ReverseInterface.java
@@ -0,0 +1,6 @@
+package cat.itacademy.s1_08.n1;
+
+@FunctionalInterface
+public interface ReverseInterface {
+    String reverse(String input);
+}
